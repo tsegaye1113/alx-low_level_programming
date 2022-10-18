ef397b3242f81d53358444fdf5ff2c815359282b
Tsegaye <tsegaye1113@gmail.com>
2022-10-18 16:40:05 +0300

    commit for all task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -7,26 +6,24 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-        int count;
-        unsigned long fib1 = 0, fib2 = 1, sum;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-        for (count = 0; count < 50; count++)
-        {
-                sum = fib1 + fib2;
-                printf("%lu", sum);
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-                fib1 = fib2;
-                fib2 = sum;
+		fib1 = fib2;
+		fib2 = sum;
 
-                if (count == 49)
-                        printf("\n");
-                else
-                        printf(", ");
-        }
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
 
-        return (0);
+	return (0);
 }
-
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,37 +10,38 @@
 
 int main(void)
 {
-        unsigned long int i;
-        unsigned long int be = 1;
-        unsigned long int af = 2;
-        unsigned long int l = 1000000000;
-        unsigned long int be1;
-        unsigned long int be2;
-        unsigned long int af1;
-        unsigned long int af2;
-printf("%lu", be);
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-        for (i = 1; i < 91; i++)
-        {
-                printf(", %lu", af);
-                af += be;
-                be = af - be;
-        }
+	printf("%lu", bef);
 
-        be1 = (be / l);
-        be2 = (be % l);
-        af1 = (af / l);
-        af2 = (af % l);
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
 
-        for (i = 92; i < 99; ++i)
-        {
-                printf(", %lu", af1 + (af2 / l));
-                printf("%lu", af2 % l);
-                af1 = af1 + be1;
-                be1 = af1 - be1;
-                af2 = af2 + be2;
-                be2 = af2 - be2;
-        }
-        printf("\n");
-        return (0);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
 }
