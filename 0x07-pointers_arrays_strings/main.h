6c6dd00cefbd0b3eca68ea38c9d3963b4b3d0a3d
Tsegaye <tsegaye1113@gmail.com>
2022-10-31 13:07:48 +0300

    commit again

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
-int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+int _putchar(char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
